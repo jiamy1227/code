17d0a9339522a7c569f0f0bcdb07fb8dc3518594
jiamy1227 <1049465510@qq.com>
2020-07-14 00:50:26 +0800

    click事件没有绑定上

---
+++ gshop-client/src/components/star/Star.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="star" :class="`star-${size}`">
+    <span class="star-item" v-for="(starClass,index) in starClasses" :class="starClass" :key="index"></span>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Star",
+        props:{
+          score:Number,
+          size:Number
+        },
+      computed:{
+        starClasses(){
+          const {score} = this;
+          let arr = [];
+          for(let i=0;i<Math.floor(score);i++){
+            arr.push("on");
+          }
+          if(Math.round(score)!==arr.length){
+            arr.push("half")
+          }
+          while(arr.length < 5){
+            arr.push("off")
+          }
+          return arr
+        }
+      }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixins.styl"
+  .star //2x图 3x图
+    float left
+    font-size 0
+    .star-item
+      display inline-block
+      background-repeat no-repeat
+    &.star-48
+      .star-item
+        width 20px
+        height 20px
+        margin-right 22px
+        background-size 20px 20px
+        &:last-child
+          margin-right: 0
+        &.on
+          bg-image('/static/images/stars/stars')
+        &.half
+          bg-image('/static/images/stars/star48_half')
+        &.off
+          bg-image('/static/images/stars/star48_off')
+    &.star-36
+      .star-item
+        width 15px
+        height 15px
+        margin-right 6px
+        background-size 15px 15px
+        &:last-child
+          margin-right 0
+        &.on
+          bg-image('/static/images/stars/star36_on')
+        &.half
+          bg-image('/static/images/stars/star36_half')
+        &.off
+          bg-image('/static/images/stars/star36_off')
+    &.star-24
+      .star-item
+        width 10px
+        height 10px
+        margin-right 3px
+        background-size 10px 10px
+        &:last-child
+          margin-right 0
+        &.on
+          bg-image('/static/images/stars/star24_on')
+        &.half
+          bg-image('/static/images/stars/star24_half')
+        &.off
+          bg-image('/static/images/stars/star24_off')
+</style>
--- gshop-client/src/pages/Login/Login.vue
+++ gshop-client/src/pages/Login/Login.vue
@@ -4,16 +4,18 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a href="javascript:;" :class="{on:!pswLogin}" @click="pswLogin=flase">短信登录</a>
+          <a href="javascript:;" :class="{on:pswLogin}" @click="pswLogin=true">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on:!pswLogin}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!rightPhone" class="get_verification"
+                :class="{right_phone:rightPhone}" @click.prevent="getCode">
+                {{computeTime?"已发送"+computeTime:"获取验证码"}}</button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -23,16 +25,17 @@
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on:pswLogin}">
             <section>
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input type="text" maxlength="8" placeholder="密码" v-model="password" v-if="showPassword">
+                <input type="password" maxlength="8" placeholder="密码" v-model="password" v-if="!showPassword">
+                <div class="switch_button" :class="showPassword ? 'on':'off'" @click="showPasword=!showPasword">
+                  <div class="switch_circle" :clas="{right: showPasword}"></div>
+                  <span class="switch_text">{{showPasword ? 'abc' : '.'}}</span>
                 </div>
               </section>
               <section class="login_message">
@@ -54,7 +57,35 @@
 
 <script>
     export default {
-        name: "Login"
+        name: "Login",
+      data(){
+          return {
+            pswLogin:false,
+            phone:'',
+            computeTime:0,
+            password:'',
+            showPassword:false,
+
+          }
+      },
+      computed:{
+        rightPhone(){
+          return /^1\d{10}$/.test(this.phone)
+        }
+      },
+      methods:{
+        async getCode(){
+          if(!this.computeTime){
+            this.computeTime = 30;
+            const intervalId = setInterval(()=>{
+              this.computeTime--;
+              if(this.computeTime<=0){
+                clearInterval(intervalId)
+              }
+            },1000)
+          }
+        }
+      }
     }
 </script>
 
@@ -119,7 +150,9 @@
                 color #ccc
                 font-size 14px
                 background transparent
-            .login_verification
+                &.right_phone
+                 color: black
+          .login_verification
               position relative
               margin-top 16px
               height 48px
